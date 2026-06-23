7bb85e871a4e92ae52275db2df723b09a1812fb5
Eric Benson <ebseattle@gmail.com>
2026-06-23 01:31:45 -0700

    fixed range-data

--- TeX/book-to-tex.py
+++ TeX/book-to-tex.py
@@ -1,6 +1,6 @@
 # Convert a PDF-assembling .tex file into a .book file to use as input to lilybook.py
 
-import sys, pathlib, re, os
+import sys, pathlib, re, os, pypdf
 
 arg_filename = sys.argv[1]
 source_path = pathlib.Path(arg_filename)
@@ -10,47 +10,135 @@ if not (source_path.is_file() and os.access(source_path, os.R_OK)):
     if not (source_path.is_file() and os.access(source_path, os.R_OK)):
         sys.exit(f"{arg_filename} is not a readable book file")
 
+book_filename = source_path.with_suffix(".book")
 tex_filename = source_path.with_suffix(".tex")
 
 book_title = 'Table of Contents'
 
-titles = []
-ly_files = []
+title = False
+titles = [];
+ly_files = [];
 
-with open(source_path) as tex_file:
-    for l in tex_file:
+include_next = False
+
+with open(book_filename) as book_file:
+    for l in book_file:
         line = l.strip()
         if line:
-            match = re.search('\\\\renewcommand\\{\\\\contentsname\\}\\{([^\\}]+)\\}', line)
-            if match:
-                book_title = match.group(1)
+            if not title:
+                title = line
+                book_title = title
+            elif include_next:
+                ly_files.append(line)
+                include_next = False
             else:
-                match = re.search('song\\{([^\\}]+)\\}\\{"([^"]+)"\\}', line)
-                if match:
-                    titles.append(match.group(1))
-                    pdf_files.append(match.group(2))
+                titles.append(line)
+                include_next = True
 
-print(book_title)
-print(titles)
-print(ly_files)
+destination_matches = {
+    "Bass Line for Standard": "../Standard/Bass Line/",
+    "Guitar Solo for Standard": "../Standard/Guitar Solo/",
+    "Bass for Standard": "../Standard/Bass/",
+    "Bass High for Standard": "../Standard/Bass/",
+    "Bass Low for Standard": "../Standard/Bass/",
+    "Bass for Alto Voice": "../Alto Voice/Bass/",
+    "for Bb for Alto Voice": "../Alto Voice/Bb/",
+    "for Bb High for Alto Voice": "../Alto Voice/Bb/",
+    "for Bb Low for Alto Voice": "../Alto Voice/Bb/",
+    "Bass for Baritone Voice": "../Baritone Voice/Bass/",
+    "for Bb for Baritone Voice": "../Baritone Voice/Bb/",
+    "for Bb High for Baritone Voice": "../Baritone Voice/Bb/",
+    "for Bb Low for Baritone Voice": "../Baritone Voice/Bb/",
+    "for Bb for Standard": "../Standard/Bb/",
+    "for Bb High for Standard": "../Standard/Bb/",
+    "for Bb Low for Standard": "../Standard/Bb/",
+    "for Eb for Standard": "../Standard/Eb/",
+    "for Eb for Alto Voice": "../Alto Voice/Eb/",
+    "for Eb for Baritone Voice": "../Baritone Voice/Eb/",
+    "Standard": "../Standard/",
+    "Alto Voice": "../Alto Voice/",
+    "Baritone Voice": "../Baritone Voice/",
+    "for Viola": "../Standard/Viola/"
+}
+
+destination_default = "../Others/"
+
+def source_to_dest(file):
+    match = re.search("/([^/]+)$", str(file) )
+    if match:
+        base_file = match.group(1)
+        for filetail, destdir in destination_matches.items():
+            if base_file.endswith(filetail):
+                return destdir + base_file
+        return destination_default + base_file
+    sys:exit(f"Unknown entry {file} in book file")
 
 pdf_files = []
 
 # convert files from ly path to pdf path
 for file in ly_files:
-    match = re.search("^../Wrappers/(.+)$", file)
-    if not match:
-        sys:exit(f"Unrecognized file {file}")
-    dir = match.group(1)
-    pdf_file = match.group(2)
-    if "Chords" in dir:
-        sys:exit(f"Unable to convert chords files {file}")
-    ly_files.append('../Wrappers/' + pdf_file + ".ly")
+    path = pathlib.Path(file)
+    no_suffix_path = path.with_suffix("")
+    if str(no_suffix_path).startswith("../Wrappers/"):
+        pdf_files.append(source_to_dest(no_suffix_path))
+    else:
+        pdf_files.append(no_suffix_path)
+    
 
-print(ly_files)
+texfile_start = f"""
+\\include{{../Include/songbook.tex}}
 
-with open(book_filename, "w") as f:
-    f.write(book_title + '\n\n')
-    for title, file in zip(titles, ly_files):
-        f.write(title + '\n')
-        f.write(file + '\n\n')
+\\begin{{document}}
+
+\\phantomsection
+\\renewcommand{{\\contentsname}}{{{book_title}}}
+\\setlength{{\\cftbeforesecskip}}{{0cm}}
+\\renewcommand{{\\cftsecfont}}{{\\small}}
+\\addcontentsline{{toc}}{{section}}{{Table of Contents}}
+\\pdfbookmark{{Table of Contents}}{{Table of Contents}}
+\\tableofcontents
+\\cleardoublepage
+
+"""
+
+texfile_end = """
+\\end{{document}}
+"""
+
+def make_song_spec(file, title, previous_title_prefix):
+    pdf_path = pathlib.Path(file).with_suffix(".pdf")
+    page_count = 1
+    same_title = title.startswith(previous_title_prefix)
+    print(pdf_path)
+    print(page_count)
+    print(previous_title_prefix)
+    print(f"is file {pdf_path.is_file()}")
+    print(f"is readable {os.access(pdf_path, os.R_OK)}")
+    if pdf_path.is_file() and os.access(pdf_path, os.R_OK):
+        reader = pypdf.PdfReader(pdf_path)
+        page_count = len(reader.pages)
+    
+
+    if page_count > 1:
+        if same_title:
+            return "dxsong"
+        else:
+            return "dsong"
+    else:
+        if same_title:
+            return "xsong"
+        else:
+            return "song"
+
+with open(tex_filename, "w") as f:
+    f.write(texfile_start)
+    previous_title_prefix = 'No Such Title'
+    for title, file in zip(titles, pdf_files):
+        songspec = make_song_spec(file, title, previous_title_prefix)
+        f.write(f'\\{songspec}{{{title}}}{{"{file}"}}\n\n')
+        match = re.search("^([^-]+ - )", title)
+        if match:
+            previous_title_prefix = match.group(1)
+        else:
+            previous_title_prefix = 'No Such Title'
+    f.write(texfile_end)
